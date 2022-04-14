b8029b056348fe4eb0eef518daeba481f9b47a99
mattBraden1 <98129256+mattBraden1@users.noreply.github.com>
2022-04-13 20:45:49 -0400

    Update testSelectionSort.java

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -11,17 +11,10 @@ import org.junit.Test;
 public class testSelectionSort {
 @Test
 public void test() {
-<<<<<<< HEAD
 testPositive();
 testNegative();
-//testMixed();
-//testDuplicates();
-=======
-//testPositive();
-//testNegative();
 testMixed();
 testDuplicates();
->>>>>>> mixed+dupes
 }
     public testSelectionSort() {
     }

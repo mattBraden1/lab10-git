6effcd06926c4f13a6409fb2d4b9399277efb29d
matt <mjbraden@umich.edu>
2022-04-13 20:33:49 -0400

    added new positive and negative test branch

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -13,12 +13,12 @@ public class testSelectionSort {
 public void test() {
 testPositive();
 testNegative();
-testMixed();
-testDuplicates();
+//testMixed();
+//testDuplicates();
 }
     public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -26,19 +26,19 @@ testDuplicates();
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] sortedArr = new int[5];
         sortedArr[0] = 2;
         sortedArr[1] = 7;
         sortedArr[2] = 8;
         sortedArr[3] = 9;
         sortedArr[4] = 10;
-        
+
         SelectionSort test = new SelectionSort();
     	int[] selectionArr = test.basicSelectionSort(arr);
     	assertArrayEquals("The array was not correctly sorted", selectionArr, sortedArr);
     }
-    
+
     public void testNegative(){
     	int[] arr = new int[5];
         arr[0] = -8;
@@ -46,19 +46,19 @@ testDuplicates();
         arr[2] = -7;
         arr[3] = -10;
         arr[4] = -2;
-        
+
         int[] sortedArr = new int[5];
         sortedArr[0] = -10;
         sortedArr[1] = -9;
         sortedArr[2] = -8;
         sortedArr[3] = -7;
         sortedArr[4] = -2;
-        
+
         SelectionSort test = new SelectionSort();
     	int[] selectionArr = test.basicSelectionSort(arr);
     	assertArrayEquals("The array was not correctly sorted", selectionArr, sortedArr);
     }
-    
+
     public void testMixed(){
     	int[] arr = new int[5];
         arr[0] = 6;
@@ -66,19 +66,19 @@ testDuplicates();
         arr[2] = 0;
         arr[3] = -13;
         arr[4] = 1;
-        
+
         int[] sortedArr = new int[5];
         sortedArr[0] = -13;
         sortedArr[1] = -4;
         sortedArr[2] = 0;
         sortedArr[3] = 1;
         sortedArr[4] = 6;
-        
+
         SelectionSort test = new SelectionSort();
     	int[] selectionArr = test.basicSelectionSort(arr);
     	assertArrayEquals("The array was not correctly sorted", selectionArr, sortedArr);
     }
-    
+
     public void testDuplicates(){
         int[] arr = new int[5];
         arr[0] = 7;
@@ -86,14 +86,14 @@ testDuplicates();
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = -2;
-        
+
         int[] sortedArr = new int[5];
         sortedArr[0] = -2;
         sortedArr[1] = 7;
         sortedArr[2] = 7;
         sortedArr[3] = 7;
         sortedArr[4] = 10;
-        
+
         SelectionSort test = new SelectionSort();
     	int[] selectionArr = test.basicSelectionSort(arr);
     	assertArrayEquals("The array was not correctly sorted", selectionArr, sortedArr);
